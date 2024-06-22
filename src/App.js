efb41123a96efeba7764bca7e4e8f015d4c70288
Aditya-Singh-5014 <Aditya.singh.050204@gmail.com>
2024-06-22 15:35:54 +0530

    for deploying

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import SurveyForm from "./components/SurveyForm";
+import "./styles/SurveyForm.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <SurveyForm />
+    </div>
+  );
+};
+
+export default App;
